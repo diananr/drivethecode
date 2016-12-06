da3b2fb92f0b39d7e71e04f2d0e65405ebd1bfa4
Karen os <karenestelaos@gmail.com>
2016-12-06 07:36:52 -0500

    header styles changes

--- public/js/script.js
+++ public/js/script.js
@@ -30,18 +30,14 @@ var good = function(pos){
 	   	}
     };
     
-    var marker = new google.maps.Marker({
-        position: latlon,
-        map: map,
-        title: 'PEPSized Coffee',
-        icon: {
-            url: "images/markers/svg/Coffee_3.svg",
-            scaledSize: new google.maps.Size(64, 64)
-        }
-    });
-
-    
     var map = new google.maps.Map(document.getElementById("map"), myOptions);
+    
+    var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/parking_lot_maps.png';
+      var marker = new google.maps.Marker({
+        position: loatlon,
+        map: map,
+        icon: iconBase 
+    });
 }
 var error = function(error){
 	console.log(error);
@@ -96,4 +92,35 @@ var autocomplete = function() {
 	var autocomplete_destination = new google.maps.places.Autocomplete(destination_input, options);
 }
 
-////////////
+
+
+var addressValidator = require('address-validator');
+var Address = addressValidator.Address;
+var _ = require('underscore');
+
+//any of the props in this object are optional, also spelling does not have to be exact.
+var address = new Address({
+    street: '100 North Washington St',
+    city: 'Bostont',
+    state: 'Mass',
+    country: 'US'
+});
+
+//the passed in address does not need to be an address object it can be a string. (address objects will give you a better likelihood of finding an exact match)
+address = '100 North Washington St, Boston, MA, US';
+
+//`addressValidator.match.streetAddress` -> tells the validator that you think the input should be a street address. This data makes the validator more accurate. 
+// But, sometimes you dont know.. in that cause you should use `addressValidator.match.unknown`
+addressValidator.validate(address, addressValidator.match.streetAddress, function(err, exact, inexact){
+    console.log('input: ', address.toString())
+    console.log('match: ', _.map(exact, function(a) {
+      return a.toString();
+    }));
+    console.log('did you mean: ', _.map(inexact, function(a) {
+      return a.toString();
+    }));
+
+    //access some props on the exact match
+    var first = exact[0];
+    console.log(first.streetNumber + ' '+ first.street);
+});
