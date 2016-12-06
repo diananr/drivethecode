97529bb56633a9c24a8e87bfcf466e4f8a96a527
AmySeo12 <maryori.amy@gmail.com>
2016-12-06 11:33:55 -0500

    add cost

--- public/js/script.js
+++ public/js/script.js
@@ -67,6 +67,7 @@ var error = function(error){
 //show route
 var showRoute = function(){
 	if($("#startPoint").val().trim().length > 0 && $("#endPoint").val().trim().length > 0){
+		console.log(typeof start_lat, start_lat);
 		var directionsDisplay = new google.maps.DirectionsRenderer({
 			polylineOptions: {
 	      		strokeColor: "#9194a1"
@@ -142,22 +143,29 @@ var showRoute = function(){
 		$.ajax({
 			url: 'https://api.lyft.com/v1/cost',
 			data:{
-		    	start_lat : start_lat,
-		    	start_lng : start_lng,
-		    	end_lat : end_lat,
-		    	end_lng : end_lng
+		    	start_lat : 37.7772,
+		    	start_lng : -122.4233,
+		    	end_lat : 37.7972,
+		    	end_lng : -122.4533
 		    	//'https://api.lyft.com/v1/cost?start_lat=37.7772&start_lng=-122.4233&end_lat=37.7972&end_lng=-122.4533'
 		    },
 			beforeSend: function(xhr) {
 				xhr.setRequestHeader("Authorization", "bearer " + access_token);
 	        },
 			success: function(response){
-				console.log(response);
-		    },
+				console.log(response.cost_estimates[0]);
+				localStorage.setItem("lyftPlus", response.cost_estimates[0].estimated_cost_cents_min + " - " + response.cost_estimates[0].estimated_cost_cents_max );
+				localStorage.setItem("lyftLine", response.cost_estimates[1].estimated_cost_cents_min + " - " + response.cost_estimates[1].estimated_cost_cents_max );
+				localStorage.setItem("lyft", response.cost_estimates[2].estimated_cost_cents_min + " - " + response.cost_estimates[2].estimated_cost_cents_max );
+		    },	
 		    error: function(error){
 		    	console.log(error);
 		    }
-		});
+		})
+
+		$("#lyftPlus").html(localStorage.getItem("lyftPlus"));
+		$("#lyftLine").html(localStorage.getItem("lyftLine"));
+		$("#lyft").html(localStorage.getItem("lyft"));
 	}
 }
 
